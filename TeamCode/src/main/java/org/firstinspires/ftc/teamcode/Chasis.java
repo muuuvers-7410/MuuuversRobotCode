097709b023c88bd3ac4f32bd4afb0cea67ffaeaf
Alejandro-2009 <24419070310248@cecytenl.edu.mx>
2026-04-17 14:52:40 -0600

    En el disparador lo actualize para que dispare con el trigger derecho y voten por el PRI

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chasis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chasis.java
@@ -1,31 +1,4 @@
-/* Copyright (c) 2017 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+
 
 package org.firstinspires.ftc.teamcode;
 
@@ -64,7 +37,7 @@ public class Chasis extends LinearOpMode {
         leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
         upDrive = hardwareMap.get(DcMotor.class, "Up_drive");
-       delmedio = hardwareMap.get(DcMotor.class, "delmedio");
+       delmedio = hardwareMap.get(DcMotor.class, "del_medio");
 
 
 
@@ -81,18 +54,16 @@ public class Chasis extends LinearOpMode {
 
             double drive = -gamepad1.left_stick_y;
             double turn = gamepad1.right_stick_x;
+            double up = gamepad1.left_trigger;
 
             leftPower = Range.clip(drive + turn, -1.0, 1.0);
             rightPower = Range.clip(drive - turn, -1.0, 1.0);
+            upPower = Range.clip(gamepad1.right_trigger, -1.0, 1.0);
+
 
             leftDrive.setPower(leftPower);
             rightDrive.setPower(rightPower);
 
-            if (gamepad1.dpad_up) {
-                upPower = 1.0;
-            } else if (gamepad1.dpad_down){
-                upPower = 0.0;
-            }
             if (gamepad1.a) {
                 delmedioPower = 1.0;
             } else if (gamepad1.b){
@@ -105,10 +76,11 @@ public class Chasis extends LinearOpMode {
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Motors", "left (%1.0f), right (%1.0f), Up (%1.0f) ,delmedio (%1.0f)", leftPower, rightPower, upPower, delmedioPower);
-            telemetry.addData("voltage", "leftDriveMotor(%1.0f), rightDriveMotor(%1.0f), UpDriveMotor(%1.0f  ) ,RecogePelotasMotor(%1.0f)", leftDrive, leftPower, rightPower, upPower, delmedioPower);
+            telemetry.addData("voltage", "leftDriveMotor(%1.0f), rightDriveMotor(%1.0f), UpDriveMotor(%1.0f ), delmedioMotor(%1.0f)", leftDrive, leftPower, rightPower, upPower, delmedioPower);
             telemetry.addLine("status inicializado");
             telemetry.addData("Poder motor", "upDrive.getPower(), delmedio.getPower()");
             telemetry.update();
         }
     }
 }
+//Que vivan las chivas

3f422fbb865fccff463955efda63e56ae1a329bc
Alejandro-2009 <24419070310248@cecytenl.edu.mx>
2026-04-18 23:53:36 -0600

    Cambie el nombre del Disparador y el Recoje Pelotas a su nombre original, pero los deje igual en como los va a detectar el Driver Hub para que los detecte el Driver Hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chasis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Chasis.java
@@ -6,13 +6,9 @@ package org.firstinspires.ftc.teamcode;
 //cambios2
 //Libreria
 
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
@@ -21,14 +17,14 @@ public class Chasis extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftDrive; //Este va conectado al PIN 0
     private DcMotor rightDrive; //Este va conectado al PIN 1
-    private DcMotor upDrive; //Este va conectado al PIN 2
-    private DcMotor  delmedio; //Este va conectado al PIN 3
+    private DcMotor Disparador; //Este va conectado al PIN 2
+    private DcMotor RecojePelotas; //Este va conectado al PIN 3
 
 
     double leftPower;
     double rightPower;
-    double upPower;
-    double delmedioPower;
+    double DisparadorPower;
+    double RecojePelotasPower;
 
 
     @Override
@@ -39,15 +35,15 @@ public class Chasis extends LinearOpMode {
 
         leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
         rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-        upDrive = hardwareMap.get(DcMotor.class, "Up_drive");
-        delmedio = hardwareMap.get(DcMotor.class, "del_medio");
+        Disparador = hardwareMap.get(DcMotor.class, "Up_drive");
+        RecojePelotas = hardwareMap.get(DcMotor.class, "del_medio");
 
 
 
         leftDrive.setDirection(DcMotor.Direction.REVERSE);
         rightDrive.setDirection(DcMotor.Direction.FORWARD);
-        upDrive.setDirection(DcMotor.Direction.FORWARD);
-        delmedio.setDirection(DcMotor.Direction.FORWARD);
+        Disparador.setDirection(DcMotor.Direction.FORWARD);
+        RecojePelotas.setDirection(DcMotor.Direction.FORWARD);
 
         waitForStart();
         runtime.reset();
@@ -72,27 +68,27 @@ public class Chasis extends LinearOpMode {
             //Si se persiona "a" se encendera el Recoje Pelotas, y si se presiona "b" se apagara
 
             if (gamepad1.a) {
-                delmedioPower = 1.0;}
+                RecojePelotasPower = 1.0;}
 
              else if (gamepad1.b) {
-                delmedioPower = 0.0;}
+                RecojePelotasPower = 0.0;}
 
             //Si se presiona el "R1" se encendera el disparador, y si se presiona "R2" se apagara
 
                 if (gamepad1.right_bumper) {
-                    upPower = 1.0;}
+                    DisparadorPower = 1.0;}
 
                  else if (gamepad1.left_bumper){
-                    upPower = 0.0;}
+                    DisparadorPower = 0.0;}
 
-            upDrive.setPower(upPower);
-            delmedio.setPower(delmedioPower);
+            Disparador.setPower(DisparadorPower);
+            RecojePelotas.setPower(RecojePelotasPower);
 
 
             // Show the elapsed game time and wheel power.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("Motors", "left (%1.0f), right (%1.0f), Up (%1.0f) ,delmedio (%1.0f)", leftPower, rightPower, upPower, delmedioPower);
-            telemetry.addData("voltage", "leftDriveMotor(%1.0f), rightDriveMotor(%1.0f), UpDriveMotor(%1.0f ), delmedioMotor(%1.0f)", leftDrive, leftPower, rightPower, upPower, delmedioPower);
+            telemetry.addData("Motors", "left (%1.0f), right (%1.0f), Up (%1.0f) ,delmedio (%1.0f)", leftPower, rightPower, DisparadorPower, RecojePelotasPower);
+            telemetry.addData("voltage", "leftDriveMotor(%1.0f), rightDriveMotor(%1.0f), UpDriveMotor(%1.0f ), delmedioMotor(%1.0f)", leftDrive, leftPower, rightPower, DisparadorPower, RecojePelotasPower);
             telemetry.addLine("status inicializado");
             telemetry.addData("Poder motor", "upDrive.getPower(), delmedio.getPower()");
             telemetry.update();
